e6b28df70cbcf3298f90279ac749814e17b3a126
Charles <adeakinyemi129@gmail.com>
2021-05-29 11:32:22 +0300

    We deleted the java file

--- demz.java
+++
@@ -1,7 +0,0 @@
-public class demz {
-    public static void main(String[] args) {
-        System.out.println("Welcome Demola to java ubuntu");
-        System.out.println("Lets develop the coolest softwares ");
-    }
-    
-}
