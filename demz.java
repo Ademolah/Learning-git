6d3f4edf3f4fb882a501559f97c5ddd93a275217
Charles <adeakinyemi129@gmail.com>
2021-05-29 10:57:35 +0300

    Added a java file

---
+++ demz.java
@@ -0,0 +1,7 @@
+public class demz {
+    public static void main(String[] args) {
+        System.out.println("Welcome Demola to java ubuntu");
+        System.out.println("Lets develop the coolest softwares ");
+    }
+    
+}
